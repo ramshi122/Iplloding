72f7d8bebf17da4b081edc4be870662a8f0433c0
ramshi122 <eppyramshi@gmail.com>
2026-05-14 02:48:21 +0530

    Serve index.html from the current directory

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Render Working Successfully"
+    return send_from_directory(os.getcwd(), "index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
